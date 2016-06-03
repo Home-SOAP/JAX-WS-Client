16452de8eceebf06e38429e83e6340d621bc8706
dn200978lak <aleksandr.lazarchuk@privatbank.ua>
2016-06-03 11:41:51 +0300

    master

---
+++ src/com/javacodegeeks/enterprise/ws/WSClient.java
@@ -0,0 +1,12 @@
+package com.javacodegeeks.enterprise.ws;
+
+public class WSClient {
+
+	public static void main(String[] args) {
+        WebServiceImplService webService = new WebServiceImplService();
+        WebServiceInterface serviceInterface = webService.getWebServiceImplPort();
+
+        System.out.println("JAX-WS > " + serviceInterface.getHelloWorldAsString("[Клиент-2] This is Java Code Geeks"));
+    }
+
+}
